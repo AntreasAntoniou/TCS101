23bb2a950f8e6e781d38d3b4d7be5f7924cacbef
Antreas Antoniou <antrikohs@gmail.com>
2024-03-01 17:30:11 +0000

    add some hints and tricks

--- code/homework2.py
+++ code/homework2.py
@@ -9,10 +9,20 @@
 
 N = int(input("Enter the number of digits to print: "))
 
+# bag_of_numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+# print("The 5th number in the bag is", bag_of_numbers[4])
+
 
 def print_first_N_numbers(N):
+    bag_of_numbers = []
     for i in range(N):
         print(i)
+        bag_of_numbers.append(i)
+
+    print(bag_of_numbers)
+    sum_of_last_two_numbers = bag_of_numbers[N - 1] + bag_of_numbers[N - 2]
+    print("The sum of the last two numbers is", sum_of_last_two_numbers)
 
 
 print_first_N_numbers(N)
